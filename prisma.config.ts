5c6c80cecf20a0997ea504997dbcef06290deeb1
Aryan Kumar <aryanak9163@gmail.com>
2026-01-23 18:13:34 +0530

    adds prisma configuration

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import {config} from "dotenv"
+config({
+    path:process.cwd() + '/.env'
+})
+import {defineConfig, env} from "prisma/config"
+
+export default defineConfig({
+    schema: process.cwd() + '/prisma/schema.prisma',
+    migrations: {
+        path: process.cwd() + '/prisma/migrations',
+    },
+    datasource: {
+            url: env('DATABASE_URL')
+    }
+})
