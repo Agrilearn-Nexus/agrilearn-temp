d00ad886c859dd5a0c7fb38b6a9a796c77325aff
Aryan Kumar <aryanak9163@gmail.com>
2026-02-07 18:31:14 +0530

    update Inngest configuration and docker-compose file to refine environment variables, adjust keys, and switch app to local build
    # Conflicts:
    #	src/app/favicon.ico

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -2,8 +2,9 @@ import {Inngest} from "inngest";
 
 export const inngest = new Inngest({
     id: "registration-system",
-    baseUrl: process.env.INNGEST_BASE_URL,
-    // signingKey: process.env.INNGEST_SIGNING_KEY,
-    // eventKey:process.env.INNGEST_EVENT_KEY,
-    isDev: process.env.NODE_ENV === "development",
+    // baseUrl: process.env.INNGEST_BASE_URL,
+    signingKey: process.env.INNGEST_SIGNING_KEY,
+    eventKey: process.env.INNGEST_EVENT_KEY,
+    // isDev: process.env.INNGEST_DEV === "1",
+    isDev: false
 });
