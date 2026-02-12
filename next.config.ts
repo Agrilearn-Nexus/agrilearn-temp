efba77d011e96a088a2ac00cb5eac782e6aae528
Aryan Kumar <aryanak9163@gmail.com>
2026-02-12 22:54:58 +0530

    updates the CI/CD configuration

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,27 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactCompiler: true,
-    output: process.platform === "win32" ? undefined : "standalone",
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**",
-            },
-        ]
+  reactCompiler: true,
+  output: process.platform === "win32" ? undefined : "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
     }
+    return null;
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
 };
 
 export default nextConfig;
