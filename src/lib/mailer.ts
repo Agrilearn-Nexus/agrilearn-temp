beeb520d70e191266e9711a7316f4cd482ac0f4f
Aryan Kumar <aryanak9163@gmail.com>
2026-02-12 23:08:31 +0530

    updates mailer

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,44 +1,43 @@
 import nodemailer from "nodemailer";
-import {render} from "@react-email/render";
-import {ReactElement} from "react";
+import { render } from "@react-email/render";
+import { ReactElement } from "react";
 
 export const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST || "smtp.gmail.com",
-    port: parseInt(process.env.SMTP_PORT || "587"),
-    secure: true,
-    tls: {
-        rejectUnauthorized: false,
-        minVersion: "TLSv1.2"
-    },
-
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    },
+  host: process.env.SMTP_HOST || "smtp.gmail.com",
+  port: parseInt(process.env.SMTP_PORT || "587"),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
 });
 
 interface SendEmailOptions {
-    to: string;
-    subject: string;
-    template: ReactElement;
+  to: string;
+  subject: string;
+  template: ReactElement;
 }
 
-export const sendEmail = async ({to, subject, template}: SendEmailOptions) => {
-    try {
-        const emailHtml = await render(template);
+export const sendEmail = async ({
+  to,
+  subject,
+  template,
+}: SendEmailOptions) => {
+  try {
+    const emailHtml = await render(template);
 
-        const options = {
-            from: `AgriLearn Nexus <${process.env.SMTP_SENDER}>`,
-            to,
-            subject,
-            html: emailHtml,
-        };
+    const options = {
+      from: `AgriLearn Nexus <${process.env.SMTP_SENDER}>`,
+      to,
+      subject,
+      html: emailHtml,
+    };
 
-        const info = await transporter.sendMail(options);
-        console.log("Message sent: %s", info.messageId);
-        return {success: true, messageId: info.messageId};
-    } catch (error) {
-        console.error("Error sending email:", error);
-        throw error;
-    }
+    const info = await transporter.sendMail(options);
+    console.log("Message sent: %s", info.messageId);
+    return { success: true, messageId: info.messageId };
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
 };
